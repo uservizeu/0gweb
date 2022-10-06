578cadf8a1559eb5360c73b4fca647c410304b9a
uservizeu <112021924+uservizeu@users.noreply.github.com>
2022-10-06 14:58:50 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function comercial (){
+    window.location.href="tel:+555433147585"
+}
+
+function suporte (){
+    window.location.href="mailto:0gautomacao@gmail.com"
+}
+
+function whats (){
+    window.location.href="https://wa.me.+5554981355253"
+}
